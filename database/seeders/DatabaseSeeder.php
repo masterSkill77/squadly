e568c48f6f15c6fcc76760a2545fa0f898e37c94
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2026-05-08 20:30:19 +0300

    Fix slug not generated during seeding by removing WithoutModelEvents

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,12 +4,10 @@ namespace Database\Seeders;
 
 use App\Enums\Role;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
 
     /**
      * Seed the application's database.
