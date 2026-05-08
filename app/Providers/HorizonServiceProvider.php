3358574ca54ad694b668df50ecc2b032a837b029
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2026-05-08 20:47:49 +0300

    Register custom Sentinel horizon driver to bypass 401 in production

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -2,13 +2,23 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
+use Laravel\Sentinel\Drivers\Driver;
+use Laravel\Sentinel\Sentinel;
 
 class HorizonServiceProvider extends HorizonApplicationServiceProvider
 {
     public function boot(): void
     {
+        Sentinel::extend('horizon', fn () => new class(fn () => app()) extends Driver {
+            public function authorize(Request $request): bool
+            {
+                return true;
+            }
+        });
+
         parent::boot();
     }
 
